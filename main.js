dde1180141d4458c65fe89d62d5439e53492e536
Joel Hernandez <jhernandez98@outlook.es>
2019-06-07 09:56:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,27 @@
 const rootendpoint = 'https://api.opendota.com/api';
 
 zlFetch(`${rootendpoint}/heroStats`)
-.then(data => console.log(data.body))
+.then(data => loadHeroes(data))
 .catch(error=> console.log(error));
+
+function loadHeroes(data)
+{
+  const menu = document.querySelector(".heroes");
+  const htmlString = data.body.map(hero => 
+    {
+       return `<div class="heroe">
+       <img src="https://api.opendota.com${hero.img}"/>
+       <div> <span>${hero["localized_name"]} </span></div>
+       </div>`
+    }).join("")
+
+    menu.innerHTML = htmlString;
+}
+
+function filterHeroes(e)
+{
+    if(e.target.matches("input[type=checkbox]"))
+    {
+        console.log(e.target);
+    }
+}
