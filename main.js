73e1b1d55d3d633e881983fb60d13a4db9874c42
Joel Hernandez <jhernandez98@outlook.es>
2019-06-10 08:39:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const rootendpoint = 'https://api.opendota.com/api';
 
 zlFetch(`${rootendpoint}/heroStats`)
-.then(data=> renderHeroes(data)) 
+.then(data=> renderHeroes(data.body)) 
 .catch(error=> console.log(error));
 
 function renderHeroes(data)
 {
   const menu = document.querySelector(".heroes");
-  const htmlString = data.body.map(hero=>{
+  const htmlString = data.map(hero=>{
     return `<div class="hero">
              <img src="https://api.opendota.com${hero.img}"/>
              <div> <span>${hero["localized_name"]}  </span> </div>
@@ -24,18 +24,26 @@ function filterHeroes(e)
   {
     zlFetch(`${rootendpoint}/heroStats`)
    .then(data=> {
-     
+      const filter1 =  filterByPrimaryAttribute(data.body);
+      const filter2 =  filterByAttackType(filter1); 
+      const filter =   filterByRoles(filter2);
+      
+      renderHeroes(filter);
+
+     })
+     .catch(error=> console.log(error));
+   }  
+   
+  }
+
+  function filterByPrimaryAttribute(heroes)
+  {
     const primaryAttributeDiv = document.querySelector("#primary-attribute");
     const primaryAttributeCheckbox = Array.from(primaryAttributeDiv.querySelectorAll("input:checked"));
-    const attackTypeDiv = document.querySelector("#attack-type");
-    const attackTypeCheckboxes = Array.from(attackTypeDiv.querySelectorAll("input:checked"));
-    const rolesDiv = document.querySelector("#roles");
-    const rolesCheckBoxes = rolesDiv.querySelectorAll("input:checked");
 
-    
-      const filterHeroesList = data.body.filter(hero=>
-        {
-          if(!primaryAttributeCheckbox.length) return true
+    const filterHeroesList = heroes.filter(hero=>
+      {
+        if(!primaryAttributeCheckbox.length) return true
           
           const heroAttribute = hero["primary_attr"];
           
@@ -44,47 +52,47 @@ function filterHeroes(e)
             const attr = el.id;
             return heroAttribute===attr;
           }
-        }).filter(hero=> 
-          {
-            if(!attackTypeCheckboxes.length) return true
-
-             const heroAttack = hero["attack_type"].toLowerCase();
-              
-             for(const el of attackTypeCheckboxes)
-             {
-               const attack = el.id;
-              
-               return attack===heroAttack;
-             }
-          }).filter(hero =>
-            {
-              if(!rolesCheckBoxes.length) return true
-
-              const roles = hero["roles"];
-              
-              for(const el of rolesCheckBoxes)
-              {
-                const rol = el.id.substring(0,1).toUpperCase() + el.id.substring(1);
-              
-                if(!roles.includes(rol)) return false
-                 
-                return true;
-              }
-            });
-        const menu = document.querySelector(".heroes");
-         const htmlString = filterHeroesList.map(hero=>{
-    return `<div class="hero">
-             <img src="https://api.opendota.com${hero.img}"/>
-             <div> <span>${hero["localized_name"]}  </span> </div>
-             </div>`
-  }).join("");
-  menu.innerHTML = htmlString;
-      
-
-     })
-     .catch(error=> console.log(error));
-   }  
-   
+      })
+     return filterHeroesList;
   }
 
+  function filterByAttackType(heroes)
+  {
+    const attackTypeDiv = document.querySelector("#attack-type");
+    const attackTypeCheckboxes = Array.from(attackTypeDiv.querySelectorAll("input:checked"));
+
+     const filterHeroesList = heroes.filter(hero=>
+      {
+        if(!attackTypeCheckboxes.length) return true
+
+            const heroAttack = hero["attack_type"].toLowerCase();
+             for(const el of attackTypeCheckboxes)
+             {
+              const attack = el.id;
+              return attack===heroAttack;
+        }
+      })
+      return filterHeroesList;
+  }
+
+  function filterByRoles(heroes)
+  {
+    const rolesDiv = document.querySelector("#roles");
+    const rolesCheckBoxes = rolesDiv.querySelectorAll("input:checked");
+
+    const filterHeroesList = heroes.filter(hero=> 
+      {
+        if(!rolesCheckBoxes.length) return true
+            
+             const roles = hero["roles"];
+            for(const el of rolesCheckBoxes)
+            {
+              const rol = el.id.substring(0,1).toUpperCase() + el.id.substring(1);
+              if(!roles.includes(rol)) return false
+                  return true;
+            }
+      })
+      return filterHeroesList;
+
+  }
 
